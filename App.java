51524d2485bbb1a47c039d78fe418eec50f24b2e
studentnelsab77-ship-it <studentnelsab77@gmail.com>
2026-06-04 02:35:02 -0700

    Added Java files

---
+++ App.java
@@ -0,0 +1,17 @@
+public class App {
+    public static void main(String[] args) {
+        // Prints the original text
+        System.out.println("Hello, World!");
+
+        // I created more lines
+        System.out.println("Welcome to Java programming!");
+
+        // I createD a number variable,say,integer
+        int number1 = 5;
+        int number2 = 10;
+
+        // Did some math to print the result
+        int total = number1 + number2;
+        System.out.println("The total is: " + total);
+    }
+}
