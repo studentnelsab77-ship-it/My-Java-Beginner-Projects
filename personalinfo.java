021b14caeb2ff5b602460af54eeac59494d83bb9
studentnelsab77-ship-it <studentnelsab77@gmail.com>
2026-06-06 22:57:48 -0700

    Modified my java program

---
+++ personalinfo.java
@@ -0,0 +1,63 @@
+public class personalinformation {
+    public static void main(String[] args) {
+
+        // Strings(str) are sequence of characters.They cn be text,letters 
+        String fullName = "NELSON AMOAH ABOAGYE";
+        String level = "Level 200";
+        String nationality = "Ghanaian";
+        String hobbies = "Coding";
+        String favLanguage = "Python";
+        String favColor = "Blue";
+        String careerGoal = "Software Engineer";
+        String email = "studentnelsab77@gmail.com";
+
+        // Integer(int) is a whole number without a decimal point; csn be positive,negative, or zero
+        int age = 20;
+
+        // Character(char) is used to represent a single character 
+        char gender = 'M';
+
+        // double is used for numbers with decimal points
+        double height = 1.85;
+        double weight = 69.5;
+
+        // Boolean represents one of two values: True or false 
+        boolean isStudent = true;
+
+        // long is used for large numbers such as phone numbers
+        long phoneNumber = 233546627442L;
+
+        // Output the result 
+        System.out.println(" *** MY PERSONAL INFORMATION *** ");
+
+        System.out.println("Full Name: " + fullName);
+
+        System.out.println("Age: " + age);
+
+        System.out.println("Gender: " + gender);
+
+        System.out.println("Height: " + height + "meters");
+
+        System.out.println("Weight: " + weight + "kg");
+
+        System.out.println("Nationality: " + nationality);
+        
+        System.out.println("Student Status: " + isStudent);
+
+        System.out.println("Grade/Level: " + level);
+
+        System.out.println("Hobbies: " + hobbies );
+
+        System.out.println("Favourite Programming Language: " + favLanguage);
+
+        System.out.println("Favourite Colour: " + favColor);
+
+        System.out.println("Carrer Goal: " + careerGoal);
+
+        System.out.println("Email: " + email);
+
+        System.out.println("Phone Number: " + phoneNumber);
+        
+        
+    }
+}
